bd4381328ef110e13b30a822ea5884ceb2b87f0c
Artur <arturcic@gmail.com>
2021-08-24 14:42:36 +0200

    build images based on deps

--- build/build/Tasks/DockerBuild.cs
+++ build/build/Tasks/DockerBuild.cs
@@ -10,12 +10,6 @@ using Cake.Frosting;
 [TaskDescription("Builds the docker images")]
 public sealed class DockerBuild : FrostingTask<BuildContext>
 {
-    private const string EnvScript = @"
-ENV PATH=$PATH:/usr/local/bin:/root/.dotnet/tools \
-    DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true \
-    DOTNET_NOLOGO=true \
-    DOTNET_ROOT=/usr/local/bin";
-
     private const string InstallScript = @"
 # install dotnet
 RUN wget https://dot.net/v1/dotnet-install.sh -O $HOME/dotnet-install.sh \
@@ -32,7 +26,7 @@ RUN wget https://dot.net/v1/dotnet-install.sh -O $HOME/dotnet-install.sh \
     private static void BuildDockerImage(ICakeContext context, DockerImage dockerImage)
     {
         var (distro, version, variant) = dockerImage;
-        var workDir = DirectoryPath.FromString($"./src/linux/{distro}");
+        var workDir = DirectoryPath.FromString($"./src/linux");
 
         var dockerhubTags = dockerImage.GetDockerTagsForRepository(Constants.DockerHubRegistry);
         var githubTags = dockerImage.GetDockerTagsForRepository(Constants.GitHubContainerRegistry);
@@ -40,22 +34,24 @@ RUN wget https://dot.net/v1/dotnet-install.sh -O $HOME/dotnet-install.sh \
         var dockerfile = $"{workDir}/Dockerfile";
         var content = new StringBuilder(context.FileReadText(dockerfile));
 
-        content.AppendLine(EnvScript);
+        content.AppendLine();
+        content.Append(InstallScript);
+        if (variant == "runtime")
+        {
+            content.Append(" --runtime dotnet");
+        }
+        content.AppendLine();
+        content.AppendLine();
 
         if (variant == "sdk")
         {
-            content.AppendLine(InstallScript);
             content.AppendLine("RUN dotnet tool install powershell --global");
             if (version == "3.1")
                 content.Append(" --version 7.0.3");
 
             content.AppendLine("RUN ln -sf /root/.dotnet/tools/pwsh /usr/bin/pwsh");
+            content.AppendLine();
         }
-        else
-        {
-            content.Append(InstallScript).AppendLine(" --runtime dotnet");
-        }
-
         content.AppendLine("WORKDIR /app");
 
         context.FileWriteText($"{workDir}/Dockerfile.build", content.ToString());
@@ -65,7 +61,7 @@ RUN wget https://dot.net/v1/dotnet-install.sh -O $HOME/dotnet-install.sh \
             Rm = true,
             Tag = dockerhubTags.Union(githubTags).ToArray(),
             File = $"{workDir}/Dockerfile.build",
-            BuildArg = new[] { $"DOTNET_VERSION={version}" },
+            BuildArg = new[] { $"DOTNET_VERSION={version}", $"TAG={distro}", $"BASE_IMAGE={Constants.GitHubContainerRegistry}/{Constants.DockerImageDeps}" },
             Platform = "linux/amd64",
             Pull = true,
         };
--- build/build/Utils/Constants.cs
+++ build/build/Utils/Constants.cs
@@ -1,8 +1,8 @@
 public class Constants
 {
     public const string ToolsDirectory = "./tools";
-    public static readonly string DockerHubRegistry = $"docker.io";
-    public static readonly string GitHubContainerRegistry = $"ghcr.io";
+    public const string DockerHubRegistry = "docker.io";
+    public const string GitHubContainerRegistry = "ghcr.io";
     public const string DockerImageName = "gittools/build-images";
     public const string DockerImageDeps = "gittools/deps";
 }
