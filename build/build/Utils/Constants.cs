e743a453a65cf425936853b25ffbc2801160ba82
Artur <arturcic@gmail.com>
2022-10-20 11:00:10 +0200

    add alpine.3.15, set net 6.0 as the default, include .net 7 into build

--- build/build/Utils/Constants.cs
+++ build/build/Utils/Constants.cs
@@ -6,9 +6,9 @@ public class Constants
     public const string DockerImageName = "gittools/build-images";
     public const string DockerImageDeps = "gittools/deps";
 
-    public static readonly string VersionForDockerLatest = "5.0";
+    public static readonly string VersionForDockerLatest = "6.0";
     public static readonly string[] DistrosToSkip = { "alpine.3.12", "alpine.3.13", "alpine.3.14" };
-    public static readonly string[] VersionsToBuild = { "6.0", "5.0", "3.1" };
+    public static readonly string[] VersionsToBuild = { "7.0", "6.0", "5.0", "3.1" };
     public static readonly string[] VariantsToBuild = { "sdk", "runtime" };
     public static readonly Architecture[] ArchToBuild = { Architecture.Amd64, Architecture.Arm64 };
     public static readonly string[] DockerDistrosToBuild =
