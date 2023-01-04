0497eb168233938f00538296254f9cec25c1f4ed
Artur <arturcic@gmail.com>
2023-01-04 13:36:59 +0100

    remove obsolete distros

--- build/build/Utils/Constants.cs
+++ build/build/Utils/Constants.cs
@@ -7,18 +7,17 @@ public class Constants
     public const string DockerImageDeps = "gittools/deps";
 
     public static readonly string VersionForDockerLatest = "6.0";
-    public static readonly string[] DistrosToSkip = { "alpine.3.12", "alpine.3.13", "alpine.3.14" };
+    public static readonly string[] DistrosToSkip = { "alpine.3.13", "alpine.3.14" };
     public static readonly string[] VersionsToBuild = { "7.0", "6.0" };
     public static readonly string[] VariantsToBuild = { "sdk", "runtime" };
     public static readonly Architecture[] ArchToBuild = { Architecture.Amd64, Architecture.Arm64 };
     public static readonly string[] DockerDistrosToBuild =
     {
-        "alpine.3.12",
         "alpine.3.13",
         "alpine.3.14",
+        "alpine.3.15",
         "centos.7",
         "centos.8",
-        "debian.9",
         "debian.10",
         "debian.11",
         "fedora.33",
