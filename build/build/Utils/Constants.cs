45d741e29b811070a3f44cbe85961016c1c0b546
Artur Stolear <artur.stolear@gmail.com>
2024-04-09 21:46:16 +0200

    set VersionForDockerLatest as 8.0

--- build/build/Utils/Constants.cs
+++ build/build/Utils/Constants.cs
@@ -9,7 +9,7 @@ public static class Constants
     public const string DockerImageDeps = "gittools/deps";
 
     public static readonly Architecture[] ArchToBuild = [Architecture.Amd64, Architecture.Arm64];
-    public static readonly string VersionForDockerLatest = "6.0";
+    public static readonly string VersionForDockerLatest = "8.0";
     public static readonly string[] VersionsToBuild = ["8.0", "7.0", "6.0"];
     public static readonly string[] VariantsToBuild = ["sdk", "runtime"];
     public static readonly string[] DockerDistrosToBuild =
